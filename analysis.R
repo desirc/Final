467fccc747c0f1c7099eca167a7a16a728f0a40c
Nhat Pham <hoangnhat123456@live.com>
2019-03-11 00:36:48 -0700

    Finish analysis.

--- analysis.R
+++ analysis.R
@@ -5,6 +5,10 @@ library(httr)
 library(jsonlite)
 library(data.table)
 library(purrr)
+library(fuzzyjoin)
+library(tools)
+library(ggplot2)
+
 
 source("keys.R")
 
@@ -20,91 +24,63 @@ df <- read.csv(
   mutate(
     Grade = as.numeric(Grade),
     Longitude = as.numeric(Longitude),
-    Latitude = as.numeric(Latitude)
+    Latitude = as.numeric(Latitude),
+    Inspection.Score = as.numeric(Inspection.Score)
   )
 
 
 df_by_inspection <- df %>% 
-  group_by(Name, Longitude, Latitude, Inspection_Serial_Num) %>%
+  group_by(
+    Name, Longitude, Latitude, 
+    Inspection_Serial_Num
+  ) %>%
   summarize(
+    "Inspection Score" = mean(Inspection.Score, na.rm = TRUE),
     "Grade" = mean(Grade)
   ) %>% 
   group_by(Name, Longitude, Latitude) %>% 
-  summarise("Grade" = mean(Grade)) %>% 
+  summarise(
+    "Mean Inspection Score" = mean(`Inspection Score`, na.rm = TRUE),
+    "Mean Grade" = mean(Grade)
+  ) %>% 
   as_data_frame()
 
 
-pal <- colorNumeric(
-  palette = c("red", "yellow", "orange", "green"),
-  domain = df_by_inspection$Grade
-)
 
-leaflet() %>% 
-  addProviderTiles("CartoDB") %>% 
-  addCircleMarkers(
-    data = df_by_inspection,
-    lat = ~Latitude, 
-    lng = ~Longitude,
-    radius = 5,
-    color = ~pal(Grade),
-    label = ~Name
-  )
+yelp <- read.csv("./data/yelp.csv", stringsAsFactors = FALSE) 
 
-# BURKE GILMAN BREWING COMPANY -122.28802 47.6615
+colnames(yelp)
+colnames(df_by_inspection)
 
-get_restaurant_info("BURKE GILMAN BREWING COMPANY", -122.28802, 47.6615)
+df_merged <- df_by_inspection %>% 
+  mutate(
+    Name = toTitleCase(tolower(Name))
+  ) %>% 
+  regex_inner_join(
+    yelp,
+    by = c(
+      Name = "name"
+    )
+  ) %>% 
+  select(-Name) %>%
+  rename(Name = name) %>% as.data.frame()
 
+high_score <- df_merged %>% 
+  filter(`Mean Inspection Score` > 5) %>% 
+  select(rating) %>% pull()
 
-base_uri <- "https://api.yelp.com/v3"
-get_restaurant_info <- function(name, long, lat) {
-  endpoint <- "/businesses/search"
-  resource_uri <- paste0(base_uri, endpoint)
-  query_params <- list(
-    term = name,
-    latitude = lat,
-    longitude = long
-  )
-  
-  response <- GET(
-    resource_uri,
-    query = query_params,
-    add_headers(Authorization = paste("bearer", api_key))
-  )
-  response_text <- content(response, type = "text")
-  response_data <- fromJSON(response_text)
-  
-  # search results sort by match
-  df_business <- response_data$businesses
-  df_business
-  
-}
+low_score <- df_merged %>% 
+  filter(`Mean Inspection Score` <= 5) %>% 
+  select(rating) %>% pull()
 
-nrow(df_by_inspection)
+test_result <- t.test(x = high_score, y = low_score, alternative = "less")
+p_val <- test_result$p.value
 
+ggplot(df_merged, aes(`Mean Inspection Score`, rating)) +
+  geom_hex()
 
-df_list = list()
-ind = 1
-for (row in 1:nrow(df_by_inspection)) {
-  name <- df_by_inspection[row, 'Name'] %>% pull()
-  long <- df_by_inspection[row, 'Longitude'] %>% pull()
-  lat <- df_by_inspection[row, 'Latitude'] %>% pull()
-  
-  df <- get_restaurant_info(name, long, lat)
-  if (length(df) != 0) {
-    df_list[[ind]] <- df[1, ] %>% 
-      jsonlite::flatten() %>% 
-      mutate(
-        price = ifelse("price" %in% names(.), price, NA)
-      ) %>% 
-      select(
-        name, coordinates.latitude, coordinates.longitude, 
-        price, review_count, rating, is_closed
-      )
-    ind = ind + 1
-  }
-}
-
-yelp <- bind_rows(df_list)
-write.csv(yelp, "./data/yelp.csv")
-
-
+cor.test(
+  x = df_merged$`Mean Inspection Score`,
+  y = df_merged$rating,
+  method = "spearman"
+)$estimate
